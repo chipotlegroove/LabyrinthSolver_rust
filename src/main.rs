0bb00960b329f2cb2ed05e63ce363ce3c7d4b6b7
Jared <jaredbarojas90@gmail.com>
2023-03-07 00:33:39 -0700

    lectura archivo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
+use text_io::read;
+
+fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
+    let file = File::open(filename).expect("El archivo seleccionado no existe.");
+    return io::BufReader::new(file).lines();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Seleccione el laberinto a utilizar (sin la extensión del archivo)");
+    let nombre_laberinto:String = read!();
+    let mut ruta_laberinto:String = "src/laberintos/".to_string();
+    ruta_laberinto.push_str(&nombre_laberinto);
+    ruta_laberinto.push_str(".txt");
+    let lines = read_lines(ruta_laberinto);
+    for line in lines{
+        println!("{}",line.unwrap())
+    }
 }
